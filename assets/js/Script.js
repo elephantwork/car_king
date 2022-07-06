6b03acc9c1e0e0ef0420a75c6554c437fdb00e20
Yang Chen <cobbee5566@gmail.com>
2022-07-06 09:39:54 +0800

    add: QA flow and api connection

--- assets/js/Script.js
+++ assets/js/Script.js
@@ -1,118 +1,56 @@
-var qa_error = 0;
 $(function () {
     $('#twzipcode').twzipcode();
     
     var qa_data = null,
         qa_current = null,
         qa_num = 0,
-        qa_progress = 0;
+        qa_error = 0;
 
     $.ajaxSetup({
         cache: false
     });
-    $.getJSON("js/question.json?v1", function (data) {
-        qa_data = data
+
+    $.getJSON("assets/js/question.json?v1", function (data) {
+        qa_data = data;
+        initQA();
     });
 
-    $(window).resize(function () {
-        //resizeQAShadow()
-        resizeSwiperSlide('#swiper2 .swiper-container .swiper-slide')
-    })
-
-    $('#toggle-nav').click(function () {
-        $('#nav-list').fadeIn();
-        $('#nav-close').fadeIn();
+    $("#btn-tips").click(function () {
+        $("#tips-wrap").show();
     });
 
-    $('#nav-close').click(function () {
-        $('#nav-list').fadeOut();
-        $('#nav-close').fadeOut();
-    });
-
-    $('#nav-list li a').click(function () {
-        if (window.innerWidth <= 991) {
-            $('#nav-list').fadeOut();
-            $('#nav-close').fadeOut();
-        }
-    });
-
-    $('#btn-method').click(function () {
-        $(this).toggleClass('active');
-        $(this).closest('section').find('.event-method').toggleClass('active');
-        goto($('#section-4').offset().top);
-    });
-
-    $('#btn-tips').click(function () {
-        $('#tips-wrap .tips-content').addClass('active');
-    });
-
-    $('body').on('click', '.radio-wrap input[type=radio]', function () {
-        var name = $(this).attr('name');
-        $(`.radio-wrap input[type=radio][name=${name}]`).parent().removeClass('active')
-        $(this).parent().addClass('active');
-    });
-
-    $('#qa-content .options').on('click', 'li label', function () {
+    $('#qa-content .options').on('click', 'li input', function () {
         var answer = $(this).data('answer');
 
         if (answer) {
-            if (qa_num + 1 >= qa_current.question.length) {
-                $("#csn").val(qa_progress);
-                $('#tip-wrap .tip-content').removeClass('active');
-                $('#section-qa .qa-game').removeClass('active');
-                $('#section-qa .qa-form').addClass('active');
-                //resizeQAShadow();
-                /*
-                $('#qa-topic-type li').eq(qa_progress).find('button').attr('disabled', true).text('Done!');
+            if (qa_error == 0) {
+                $("#score").val(parseInt($("#score").val()) + 1);
+            }
 
-                if (qa_progress + 1 >= 4) {
-                    $('#section-qa .qa-game').removeClass('active');
-                    $('#section-qa .qa-form').addClass('active');
-                    resizeQAShadow();
-                }
-                else {
-                    $('#section-qa').slideUp();
-                    goto($('#qa-topic-type').offset().top);
-                    qa_current = null;
-                    ++qa_progress;
-                }
-                */
+            if (qa_num + 1 >= qa_current.question.length) {
+                $("#tips-wrap").hide();
+                $("#section-qa .qa-game").hide();
+                $("#section-qa .qa-form").show();
             }
             else {
                 nextQA();
             }
         } else {
-            //console.log(typeof qa_current.question[qa_num].error == 'undefined');
-            if (typeof qa_current.question[qa_num].error == 'undefined') {
-                ++qa_error;
-                $("#errorcount").val($("#errorcount").val()+qa_num+',')
-                qa_current.question[qa_num].error = true;
-            }
-            //console.log(qa_error);
+            ++qa_error;
 
-            $(this).closest('ul').find('li button.active').removeClass('active');
-            $(this).closest('ul').find('li .text, li .symbol').remove();
-            $(this).addClass('active');
+            $(this).closest("ul").find("li.field.error").removeClass("field error");
+            $(this).closest("ul").find("li .error-wrap").remove();
+            $(this).closest("li").addClass("field error");
+
             $error = $('<div>').addClass('error-wrap');
             $error.append($('<div>').addClass('symbol'));
             $error.append($('<div>').addClass('text').text('答錯囉，請重新選擇！'));
-            $(this).after($error);
-            $('#tip-wrap .tip-content').addClass('active');
-        }
-    });
-
-    $('#start-btn').click(function() {
-        if ($('#section-qa').is(':hidden')) {
-            var _qa_current = {
-                title: qa_data.topic[0].title,
-                question: qa_data.topic[0].question
-            };
-            qa_current = JSON.parse(JSON.stringify(_qa_current));
-            initQA();
+            $(this).parent().after($error);
+            $("#tips-wrap").show();
         }
     });
     
-
+// todo 可能需要reset
 //    $('#reset-btn').click(function() {
 //        var _qa_current = {
 //            title: qa_data.topic[0].title,
@@ -123,61 +61,70 @@ $(function () {
 //    })
 
     function initQA() {
-        $("#errorcount").val('');
-    $("#sharefb").attr("href",'https://www.facebook.com/sharer/sharer.php?u=https://financeking.storm.mg/');
-    $("#shareline").attr("href",'https://social-plugins.line.me/lineit/share?url=https://financeking.storm.mg/');
+        var _qa_current = {
+            title: qa_data.topic[0].title,
+            question: qa_data.topic[0].question
+        };
+        qa_current = JSON.parse(JSON.stringify(_qa_current));
         qa_num = 0;
         qa_error = 0;
-        //$('#qa-topic').text(`[ ${qa_current.title} ]`);
         innerQA();
-        $('#section-qa .qa-finish, #section-qa .qa-form').removeClass('active');
-        $('#section-qa .qa-game').addClass('active');
-        $('#section-qa').slideDown();
-        goto($('#section-qa').offset().top);
-        //resizeQAShadow();
+        $("#section-qa .qa-form, #section-qa .brand, #section-qa .qa-finish").hide();
+        $("#section-qa .qa-game").show();
     }
 
     function nextQA() {
         ++qa_num;
         innerQA();
-        //resizeQAShadow();
     }
 
     function innerQA() {
-        $('#qa-content .num').text(qa_num + 1);
-        $('#tip-num').text(qa_num + 1);
-        $('#qa-topic').text(`[ ${qa_current.question[qa_num].title} ]`);
-        $('#qa-content .question').text(qa_current.question[qa_num].text);
-        $('#tip-wrap .tip-content').removeClass('active');
-        $('#tip-text').text(qa_current.question[qa_num].tip);
+        $("#qa-topic").text(qa_current.question[qa_num].title);
+
+        $("#qa-content .question").text("")
+            .append($("<strong>").addClass("num").text(qa_num + 1))
+            .append(qa_current.question[qa_num].text);
 
         var symbol = ['A', 'B', 'C', 'D', 'E']
-        var $options = $('#qa-content .options');
+        var $options = $("#qa-content .options");
         $options.empty();
         qa_current.question[qa_num].options.forEach(function (q, i) {
-            $options.append($('<li>').html($('<button>').attr('type', 'button').data('answer', q.answer).html(`<span>${symbol[i]}</span> ${q.text}`)));
+            $options.append(
+                $("<li>").append(
+                    $("<label>").addClass("inside-label style-label")
+                        .append($("<input>").attr("type", "radio").attr("name", "question").data("answer", q.answer))
+                        .append($("<span>").addClass("checkvalue").text(`${ symbol[i] } ${ q.text }`))
+                        .append($("<span>").addClass("checkmark"))
+                )
+            );
         });
+
+        $("#tip-num").text(qa_num + 1);
+        $("#tips-wrap").hide();
+        $("#tips-text").text(qa_current.question[qa_num].tip);
+        qa_error = 0;
     }
-    setTimeout(() => {
-        resizeSwiperSlide('#swiper2 .swiper-container .swiper-slide')
-    }, 1000);
 });
 
+function showFinish() {
+    qaCompleted();
+}
+
 function qaCompleted() {
-    var score = 9 - qa_error;
+    var score = $("#score").val();
     var urlshare='';
     var t1 = 0, t2 = '';
-    if (score <= 4) {
+    if (score <= 2) {
         t1 = 10;
         t2 = '<p>嗚嗚，你與未來移動居然是繞地球一圈的距離！”</p>'
         urlshare=1;
     }
-    else if (score >= 5 && score <= 8) {
+    else if (score >= 3 && score <= 5) {
         t1 = 50;
         t2 = '<p>哇！你與未來移動是台北到高雄的距離！”</p>';
         urlshare=2;
     }
-    else if (score >= 9 && score <= 11) {
+    else if (score >= 6 && score <= 7) {
         t1 = 80;
         t2 = '<p>太好了！你與未來移動只有一圈操場的距離！”</p>';
         urlshare=3;
@@ -192,8 +139,8 @@ function qaCompleted() {
     $('#result-num').html(t1 + '<span>%</span>')
     $('#result-text').html(t2);
     $('#section-qa').show();
-    $('#section-qa .qa-game, #section-qa .qa-form').removeClass('active');
-    $('#section-qa .qa-finish').addClass('active');
+    $("#section-qa .qa-game, #section-qa .qa-form").hide();
+    $("#section-qa .brand, #section-qa .qa-finish").show();
     $('#rpic').attr('src', '/assets/images/qa-distance'+urlshare+'.png');
     //$('#qa-topic-type li button').attr('disabled', true).text('Done!');
     //resizeQAShadow();
